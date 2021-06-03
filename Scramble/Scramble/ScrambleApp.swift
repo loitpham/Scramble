d56ce2c77dd17ba4931be71476321c02f9fa58c2
Loi Pham <loitpham@gmail.com>
2021-06-02 23:04:53 -0700

    Completed.

---
+++ Scramble/Scramble/ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrambleApp.swift
+//  Scramble
+//
+//  Created by Loi Pham on 6/2/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
